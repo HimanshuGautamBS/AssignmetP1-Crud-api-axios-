b419c3ec48c83b07c39844d5aedecdf79e7531a9
Himanshu Gautam <himanshu.gautam@berylsystems.com>
2021-02-18 15:37:25 +0530

    Added delete functionality in user list and update the new uselist at the same time with the help of axios and Filter

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,48 +3,142 @@ import {NavLink} from 'react-router-dom';
 import axios from 'axios';
 import UserInput from './UserInput';
 
-const api = axios.create({
-    baseURL: `https://jsonplaceholder.typicode.com/users`
-})
+// const api = axios.create({
+//     baseURL: `https://jsonplaceholder.typicode.com/users`
+// })
 
 export default class User extends Component {
+
+
     state = {
-        persons:[],
-    }
-
-
-    // For user Detail with the help of onClick listner
-    getData=(id)=>{
-            
-            console.log(id)
-            api.get('/').then(res=>{
-                console.log(res.data[id]);
-            })
-    }
- 
-
-    componentDidMount(){
-        api.get('/').then(res=>{
+        users: []
+      }
+      
+      componentDidMount() {
+          
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+          .then(res => {
+            const users = res.data;
+            this.setState({ users });
+          })
+      }
+      
+      deleteRow(id){
+        axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+          .then(res => {
+            console.log(res);
             console.log(res.data);
-            this.setState({
-                persons:res.data
-            });
-        });}
 
-    render() {
-       return (
-           <> 
+            // Filter works similarly as if else
+            
+            const users = this.state.users.filter(item => item.id !== id);
+            this.setState({ users });
+          })
+      
+      }
+      
+      render() {
+          console.log("render");
+        return (
+          <div>
+            <h1>React Axios Delete Request</h1>
+      
+            <table className="table table-bordered">
+                <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Action</th>
+                      <th>Details</th>
+                  </tr>
+                </thead>
+      
+                <tbody>
+                  {this.state.users.map((user) => (
+                    <tr key={user.id}>
+                      <td>{user.id}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>
+                        <button className="btn btn-danger" onClick={() => this.deleteRow(user.id)}>Delete</button>
+                      </td>
+                      <td>
+                        <button className="btn btn-danger" onClick={(e) => this.deleteRow(user.id, e)}>Details</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+      
+            </table>
+          </div>
+        )
+      }
+    
+
+
+//     state = {
+//         persons:[],
+//     }
+
+
+//     // For user Detail with the help of onClick listner
+//     getData=(id)=>{
+            
+//             console.log(id)
+//             api.get('/').then(res=>{
+//                 console.log(res.data[id]);
+//             })
+//     }
+ 
+//     // postDeleted (){
+//     //   alert("are you sure about this")
+//     //     // if(window.confirm("Are you sure about")) {
+//     //     //     api.delete('https://jsonplaceholder.typicode.com/users/${id}')
+//     //     // }   api.get('/').then(res=>{
+//     //     //     console.log(res.data[id]); console.log(res.data)
+//     //     // })   
+       
+//     // };
+
+//     deleteData=(id)=>{
+       
+//         if(window.confirm("Are you sure about")) {
+//             axios.delete('https://jsonplaceholder.typicode.com/users/${id}').then(res=>{
+//                 console.log(res);
+//                 console.log(res.data);
+//                 const persons = this.state.persons.filter(person => person.id !== id);
+//                 this.setState({ persons });
+        
+//             })
+//             // console.log("your id id :" +id+this.getData())
+//     }}
+
+//     componentDidMount(){
+//         api.get('/').then(res=>{
+//             console.log(res.data);
+//             this.setState({
+//                 persons:res.data
+//             });
+//         });}
+
+//     render() {
+//        return (
+//            <> 
                
-            <div style={{padding:'40px'}}>
-                <h1>User Page</h1> 
-                <NavLink to="/" style={{padding:'20px'}}>Home-Page</NavLink>
+//             <div style={{padding:'40px'}}>
+//                 <h1>User Page</h1> 
+//                 <NavLink to="/" style={{padding:'20px'}}>Home-Page</NavLink>
 
-                <NavLink to="/users/new">New-user</NavLink>
-                <ul >
-                {this.state.persons.map(person => <li key={person.name}   ><button onClick={()=>this.getData(person.id-1)} >{person.name}</button></li>)}
-                </ul>
-            </div>
-            <NavLink to="/users/new">New-user</NavLink>
-        </>)
+//                 <NavLink to="/users/new">New-user</NavLink>
+//                 <ul >
+//                 {this.state.persons.map(person => <li key={person.name}   ><button onClick={()=>this.getData(person.id-1)} >{person.name}</button>
+//                 <button onClick={()=>this.deleteData(person.id-1)} >Delete</button>
+                  
+//                 </li>)}
+//                 </ul>
+//             </div>
+//             <NavLink to="/users/new">New-user</NavLink>
+//         </>)
     }
-}
+// }
